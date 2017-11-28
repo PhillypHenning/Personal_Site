b8debc206df90aab8a8584b257f3d01ec36581de
PhillypHenning <phil8t8@gmail.com>
2017-11-27 20:20:19 -0500

    adding stub test

---
+++ homepage/tests.py
@@ -0,0 +1,11 @@
+import unittest
+
+def testx(x):
+    return x + 1
+
+class MyTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(testx(3),4)
+
+if __name__ == '__main__':
+unittest.main()
